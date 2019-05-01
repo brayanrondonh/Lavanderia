ba89b7c910610fea3e4143fb769f0fcdf6855228
BrayanRondonH <brayanrondonh@gmail.com>
2019-04-30 21:41:46 -0500

    Initial commit

---
+++ src/domain/Cajero.java
@@ -0,0 +1,64 @@
+package domain;
+
+public class Cajero
+{
+    private String nombreCajero;
+    private String apellidoCajero;
+    private String dniCajero;
+    private int id_cajero;
+
+    Cajero(){}
+
+    Cajero(String nombreCajero, String apellidoCajero, String dniCajero)
+    {
+        this.nombreCajero = nombreCajero;
+        this.apellidoCajero = apellidoCajero;
+        this.dniCajero = dniCajero;
+    }
+
+    public String getNombreCajero()
+    {
+        return nombreCajero;
+    }
+
+    public void setNombreCajero(String nombreCajero)
+    {
+        this.nombreCajero = nombreCajero;
+    }
+
+    public String getApellidoCajero()
+    {
+        return apellidoCajero;
+    }
+
+    public void setApellidoCajero(String apellidoCajero)
+    {
+        this.apellidoCajero = apellidoCajero;
+    }
+
+    public String getDniCajero()
+    {
+        return dniCajero;
+    }
+
+    public void setDniCajero(String dniCajero)
+    {
+        this.dniCajero = dniCajero;
+    }
+
+    public int getId_cajero()
+    {
+        return id_cajero;
+    }
+
+    public void setId_cajero(int id_cajero)
+    {
+        this.id_cajero = id_cajero;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Cajero = id_cajero: "+id_cajero+", Nombre Cajero: "+nombreCajero+", Apellido Cajero: "+apellidoCajero+", DNI Cajero: "+dniCajero;
+    }
+}
---
+++ src/domain/Factura.java
@@ -0,0 +1,81 @@
+package domain;
+
+public class Factura
+{
+    private date fecha;
+    private time hora;
+    private String nombreEmpresa;
+    private String nombreComercial;
+    private String direccion;
+    private String ruc;
+    private String telefono;
+    private String num_operacion;
+    private String num_boleta;
+    private double igv;
+    private String id_factura;
+    private boolean cancelado;
+
+    Factura(){}
+
+    //No se agregaron mas contructores ya que es una clase de solo lectura
+
+    public date getFecha()
+    {
+        return fecha;
+    }
+
+    public time getHora()
+    {
+        return hora;
+    }
+
+    public String getNombreEmpresa()
+    {
+        return nombreEmpresa;
+    }
+
+    public String getNombreComercial()
+    {
+        return nombreComercial;
+    }
+
+    public String getDireccion()
+    {
+        return direccion;
+    }
+
+    public String getRuc()
+    {
+        return ruc;
+    }
+
+    public String getTelefono()
+    {
+        return telefono;
+    }
+
+    public String getNum_operacion()
+    {
+        return num_operacion;
+    }
+
+    public String getNum_boleta()
+    {
+        return num_boleta;
+    }
+
+    public double getIgv()
+    {
+        return igv;
+    }
+
+    public String getId_factura()
+    {
+        return id_factura;
+    }
+
+    public boolean isCancelado()
+    {
+        return cancelado;
+    }
+}
